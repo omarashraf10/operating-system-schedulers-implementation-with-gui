8575d1b336ce090b60b0e5c113280d429ed77078
omarashraf10 <omarashraf465@yahoo.com>
2018-03-25 20:33:25 +0200

    supporting priority preemptive and sjf preemptive

--- os project/Form1.cs
+++ os project/Form1.cs
@@ -58,12 +58,22 @@ namespace os_project
         {
             int temparrival = Int32.Parse(textBox4.Text);
             int tempburst = Int32.Parse(textBox2.Text);
-            int temppriority = Int32.Parse(priority_text.Text);
+            
 
             process tempprocess = new process();
             tempprocess.arrival_time = temparrival;
             tempprocess.burst_time = tempburst;
-            tempprocess.priority = temppriority;
+
+            if (comboBox1.Text == "Priority (Preemptive)" || comboBox1.Text == "Priority (Non Preemptive)")
+            {
+                int temppriority = Int32.Parse(priority_text.Text);
+                tempprocess.priority = temppriority;
+            }
+
+            if (comboBox1.Text == "SJF (Preemptive)" || comboBox1.Text == "Priority (Preemptive)")
+            {
+                tempprocess.remaining_time = tempprocess.burst_time;
+            }
 
 
             processes.Add(tempprocess);
@@ -199,6 +209,121 @@ namespace os_project
                 textBox5.Text = ((float)psum / num_process).ToString();
 
             }
+
+            else if (comboBox1.Text == "SJF (Preemptive)")
+            {
+                int t =1;
+                int remaining_time = 100000;
+                process minprocess = processes[0];
+                int sjsum = 0;
+                int index = 0;
+                int n = num_process;
+                while(n>0)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (processes[i].arrival_time <t)
+                        {
+                            if (processes[i].remaining_time < remaining_time)
+                            {
+                                minprocess = processes[i];
+                                remaining_time = processes[i].remaining_time;
+                                index = i;
+                            }
+                        }
+
+                    }
+                    /*
+                    if (cpuend > minprocess.arrival_time)
+                        minprocess.waiting = cpuend - minprocess.arrival_time;
+                    else
+                        minprocess.waiting = 0;
+                    */
+
+                  //  cpuend += minprocess.burst_time;
+
+
+                    processes[index].remaining_time--;
+                    if (processes[index].remaining_time <= 0)
+                   {
+                       processes[index].waiting = t - processes[index].arrival_time - processes[index].burst_time;
+                       minprocess.waiting = processes[index].waiting;
+                       for (int i = index; i < n - 1; i++)
+                       {
+                           processes[i] = processes[i + 1];
+                       }
+                       n--;
+
+                       remaining_time = 100000;
+
+                       sjsum += minprocess.waiting;
+                   }
+                   t++;
+                }
+                textBox5.Text = ((float)sjsum / num_process).ToString();
+
+
+            }
+
+
+            else if (comboBox1.Text == "Priority (Preemptive)")
+            {
+                int t = 1;
+                int priority = 1000000;
+                process minprocess = processes[0];
+                int sjsum = 0;
+                int index = 0;
+                int n = num_process;
+                while (n > 0)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (processes[i].arrival_time < t)
+                        {
+                            if (processes[i].priority < priority)
+                            {
+                                minprocess = processes[i];
+                                priority = processes[i].priority;
+                                index = i;
+                            }
+                        }
+
+                    }
+                    /*
+                    if (cpuend > minprocess.arrival_time)
+                        minprocess.waiting = cpuend - minprocess.arrival_time;
+                    else
+                        minprocess.waiting = 0;
+                    */
+
+                    //  cpuend += minprocess.burst_time;
+
+
+                    processes[index].remaining_time--;
+                    if (processes[index].remaining_time <= 0)
+                    {
+                        processes[index].waiting = t - processes[index].arrival_time - processes[index].burst_time;
+                        minprocess.waiting = processes[index].waiting;
+                        for (int i = index; i < n - 1; i++)
+                        {
+                            processes[i] = processes[i + 1];
+                        }
+                        n--;
+
+                        priority = 100000;
+
+                        sjsum += minprocess.waiting;
+                    }
+                    t++;
+                }
+                textBox5.Text = ((float)sjsum / num_process).ToString();
+
+
+            }
+
+
+
+
         }
 
         private void priority_text_TextChanged(object sender, EventArgs e)
